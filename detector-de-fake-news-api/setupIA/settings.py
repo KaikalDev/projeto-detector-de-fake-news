90ac08d70ea74a46d08680dec32c39e28e99a8d3
Kaique <kaiquesimoes58@gmail.com>
2025-03-25 23:11:50 -0300

    Ajustes

--- detector-de-fake-news-api/setupIA/settings.py
+++ detector-de-fake-news-api/setupIA/settings.py
@@ -21,8 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
-    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
